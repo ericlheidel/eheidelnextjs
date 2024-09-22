c72e0cb481fb019429e5d3cea3e595db88e24d2f
Eric Heidel <ericleeheidel@yahoo.com>
2024-09-22 16:41:50 -0500

    color them notes in utility

--- utility.tsx
+++ utility.tsx
@@ -1,8 +1,8 @@
-export const color1 = "amber-100" // #FCECC9
-export const color2 = "amber-400" // #FFC502
-export const color3 = "orange-600" // #EA6701
-export const color4 = "red-700" // #B1000D
-export const color5 = "gray-900" // #090C02
+export const color1 = "amber-100" // #FCECC9  // beige-y
+export const color2 = "amber-400" // #FFC502  // yellow
+export const color3 = "orange-600" // #EA6701 // orange
+export const color4 = "red-700" // #B1000D    // red
+export const color5 = "gray-900" // #090C02   // black
 
 export const codingSkills = [
   {
